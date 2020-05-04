e1f28ccb02f06e4a7f591362d115e7e6170cd274
Tomasz Wojtowicz <tomekwojt@gmail.com>
2020-05-04 21:56:11 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const firstname = 'WITMAR';
+const age = 30;
+
+console.log(firstname);
+console.log(age);
+console.log(`Witaj, na stronie internetowej firmy ${firstname}, jesteśmy na rynku już ${age} lat. `)
