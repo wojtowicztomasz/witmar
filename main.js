5b4bfbdcc4201d3f598d9514aba15d1cf947705f
Tomasz Wojtowicz <tomekwojt@gmail.com>
2020-06-03 21:43:32 +0200

    witmarmobile

--- main.js
+++ main.js
@@ -6,3 +6,36 @@ console.log(age);
 console.log(
   `Witaj, na stronie internetowej firmy ${firstname}, jesteśmy na rynku już ${age} lat. `
 );
+
+function calculate(myNumber){
+  console.log(`Dostałam ${myNumber}`);
+  return myNumber*7;
+
+}
+const myResult = calculate(2);
+console.log(myResult);
+
+const deathStar = {
+  diameter: 120000,
+  fire: (target) => {
+    console.log(`${target}destroyed`)
+  },
+  isOperating:true,
+  levels:357,
+  name:`deathStar`,
+  populatoin:10000,
+  isLightOn: true,
+  commander: {
+    name:`Darth Vader`,
+    age:44
+  }
+}
+
+
+
+
+const myProperty ='name'
+console.log(deathStar[myProperty])
+deathStar['name']
+
+cost showMeProperty = (myProperty)
